caf63bed6e38a90696e697ee7c3db42cb767e625
Дмитрий Панов <dmitry.panov2016@gmail.com>
2022-02-08 11:46:12 +0300

    pages: added position and employee pages

--- app/Http/Controllers/PositionsController.php
+++ app/Http/Controllers/PositionsController.php
@@ -13,7 +13,7 @@ class PositionsController extends Controller
      */
     public function index()
     {
-        //
+        return view('positions.index');
     }
 
     /**
---
+++ resources/views/positions/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Positions')
+
+@section('content_header')
+    <h1>Positions</h1>
+@stop
+
+@section('content')
+
+@stop
